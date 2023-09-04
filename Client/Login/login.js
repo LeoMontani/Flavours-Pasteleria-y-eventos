7113443f5a9f4f50c67eab01e6cab6ffe5e77f91
LeoMontani <leonel.montani1@gmail.com>
2023-09-04 16:28:00 -0300

    Tengo que resolver el problema de mis dependencias

--- Client/Login/login.js
+++ Client/Login/login.js
@@ -1,20 +1,8 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const loginForm= document.getElementById("login-form");
+const loginForm = document.getElementById("login-form");
 
-app.use(express.json());
-
-
-loginForm.addEventListener ("submit", (e) => {
+loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const email = loginForm["email"].value;
   const contraseña = loginForm["contraseña"].value;
   console.log(email, contraseña);
-})
-
-
-// Esto lo sigo después
-app.post ("/users", (req, res) => {
-
-})
+});
